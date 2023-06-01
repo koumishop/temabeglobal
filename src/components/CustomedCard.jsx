5877197aa272d84e55dd1d133c9debf18e7fa815
koumishop15 <koumishop@gmail.com>
2023-06-01 12:07:28 +0100

    fix: set custom card margin

--- src/components/CustomedCard.jsx
+++ src/components/CustomedCard.jsx
@@ -2,23 +2,10 @@ import Card from 'react-animated-3d-card'
 
 const CustomedCard = ({width, height, onClick, children})=>{ 
 return (
-  <div className={`p-2 rounded-2xl bg-gradient-to-r from-[#312F33]-200 via-[#312F33]-400 to-[#312F33]-600 bg-[#312F33] ${width} ${height} cursor-pointer shadow-xl flex flex-col justify-center items-center hover:shadow-2xl hover:scale-110`}  onClick={onClick}>
+  <div className={`mt-2.5 p-2 rounded-2xl bg-gradient-to-r from-[#312F33]-200 via-[#312F33]-400 to-[#312F33]-600 bg-[#312F33] ${width} ${height} cursor-pointer shadow-xl flex flex-col justify-center items-center hover:shadow-2xl hover:scale-110`}  onClick={onClick}>
   {children}
   </div>
 )
-  // return <Card       
-    //   style={{
-    //     backgroundColor: '#312F33',
-    //     width: `${width}`,//'310px',
-    //     height: `${height}`,//'185px',
-    //     cursor: 'pointer',
-    //     display:'flex',
-    //     flexDirection: 'column',
-    //     justifyContent:'center',
-    //     alignItems:'center'
-    //   }}
-    //   onClick={onClick}
-    // >{children}</Card>
 }
 
 export default CustomedCard
